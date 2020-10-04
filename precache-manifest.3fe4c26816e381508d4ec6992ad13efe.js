7163fb5b15b75f8d07f1f6f1a98cab37186d0466
aman puri <puriaman846@gmail.com>
2020-10-04 13:39:27 +0530

    Updates

---
+++ precache-manifest.3fe4c26816e381508d4ec6992ad13efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9104110b574ce0687f6e68ed898ef690",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "9f9c1fc52062a312c279",
+    "url": "/reactwebsite/static/css/main.8fca6708.chunk.css"
+  },
+  {
+    "revision": "1a0d7f80bfd9c6ab69f8",
+    "url": "/reactwebsite/static/js/2.b916fbfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactwebsite/static/js/2.b916fbfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9c1fc52062a312c279",
+    "url": "/reactwebsite/static/js/main.63926bbd.chunk.js"
+  },
+  {
+    "revision": "eec87808e158f3f4700a",
+    "url": "/reactwebsite/static/js/runtime-main.824e2a87.js"
+  }
+]);
